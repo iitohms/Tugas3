2958f98f3c1da17e6d4562b8595deb45ece934ad
iitohms <masittoh.kiwkiw@gmail.com>
2019-11-20 20:43:00 +0700

    ngubah pembayaran

--- pembayaran1.php
+++ pembayaran1.php
@@ -1,7 +1,7 @@
 <?php
 include("koneksi1.php"); //fungsi untuk gabunngg file
 if (isset($_POST['simpan'])){ //post  isinya pakai harga barang dan simpan bawah submit & isset=simpan masuk ke table
-	$query_tambah_data="insert into barang(merek,harga) values
+	$query_tambah_data="insert into t_pembayaran(merek,qty,harga) values
 	('".$_POST['merek']."','".$_POST['qty']."',
 	'".$_POST['harga']."')";
 	$proses_data=mysql_query($query_tambah_data);
@@ -20,18 +20,18 @@ if($proses_data){
 	<table border="1">
 		<tr>
 			<td>Nama Barang</td>
-			<td><input name="Merek" type="text"></td>
+			<td><input name="merek" type="text"></td>
 		</tr>
 		<tr>
 			<td>Qty</td>
-			<td><input name="Qty" type="number"></td>
+			<td><input name="qty" type="number"></td>
 		</tr>
 		<tr>
 			<td>Harga</td>
-			<td><input name="Harga" type="number"></td>
+			<td><input name="harga" type="number"></td>
 		</tr>
 		<tr>
-			<td><input name="Simpan" type="submit"></td>
+			<td><input name="simpan" type="submit"></td>
 		</tr>
 	</table>
 	</form>
